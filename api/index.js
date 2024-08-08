0dd49cba8f3779954c9e0903c70e8d2676e5adf9
bright <obololastkiller@gmail.com>
2024-08-08 06:32:40 +0000

    connect to database

--- api/index.js
+++ api/index.js
@@ -1,4 +1,17 @@
-import express from "express";
+import express from "express"; // for server
+import mongoose from "mongoose"; // for database
+import dotenv from "dotenv";
+
+dotenv.config();
+
+mongoose
+  .connect(process.env.URL)
+  .then(() => {
+    console.log("MongoDb is connected");
+  })
+  .catch((err) => {
+    console.log(err);
+  });
 
 const app = express();
 
