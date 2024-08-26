629ad6ef3eb04ca87c00a398dc7e0d4bebaa160f
bright <obololastkiller@gmail.com>
2024-08-26 02:39:31 +0000

    create signin api route

--- api/controllers/auth.controller.js
+++ api/controllers/auth.controller.js
@@ -1,6 +1,7 @@
 import { errorHandler } from "../error.js";
 import User from "../models/user.model.js";
 import bcryptjs from "bcryptjs";
+import jwt from "jsonwebtoken";
 
 //Signup function
 export const signup = async (req, res, next) => {
@@ -15,7 +16,7 @@ export const signup = async (req, res, next) => {
     password === ""
   ) {
     // return res.status(400).json({ message: "All fields are required" });
-    next(errorHandler(400, "All fields are required"));
+    return next(errorHandler(400, "All fields are required"));
   }
 
   const hashedPassword = bcryptjs.hashSync(password, 10);
@@ -31,6 +32,36 @@ export const signup = async (req, res, next) => {
     res.json("Signup successful");
   } catch (error) {
     // res.status(500).json({ message: error.message });
-    next(error);
+    return next(error);
+  }
+};
+
+//Signin function
+export const signin = async (req, res, next) => {
+  const { email, password } = req.body;
+  if (!email || !password || email === "" || password.trim() === "") {
+    return next(errorHandler(400, "All fields are required"));
+  }
+  try {
+    const validUser = await User.findOne({ email });
+    const validPassword =
+      validUser && bcryptjs.compareSync(password, validUser.password);
+    if (!validUser || !validPassword) {
+      return next(errorHandler(400, "Wrong credentials"));
+    }
+    const token = jwt.sign({ id: validUser._id }, process.env.JWT_SECRET);
+    const { password: pass, ...otherFields } = validUser._doc;
+
+    res
+      .status(200)
+      .cookie("access_token", token, {
+        httpOnly: true,
+        secure: process.env.NODE_ENV === "production",
+        expires: new Date(Date.now() + 7 * 24 * 60 * 60 * 1000),
+      })
+      .json(otherFields);
+  } catch (error) {
+    console.error(error);
+    return next(error);
   }
 };
