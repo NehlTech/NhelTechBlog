4ee4000d39d9167601c5168e35ffe1c05643db5b
bright <obololastkiller@gmail.com>
2024-09-12 04:01:27 +0000

    create get post api route

--- api/routes/post.route.js
+++ api/routes/post.route.js
@@ -1,10 +1,11 @@
 import express from "express";
 
 import { verifyToken } from "../utils/verifyUser.js";
-import { createPost } from "../controllers/createPost.controller.js";
+import { createPost, getposts } from "../controllers/createPost.controller.js";
 
 const router = express.Router();
 
 router.post("/createpost", verifyToken, createPost);
+router.get("/getposts", getposts);
 
 export default router;
